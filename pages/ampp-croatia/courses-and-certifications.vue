fa8bd7679a258da006252695df5212d83a38eb1a
BacaniLeon <leonbaani@gmail.com>
2025-09-14 14:36:42 +0200

    Added new page both web and mobile view

--- pages/ampp-croatia/courses-and-certifications.vue
+++ pages/ampp-croatia/courses-and-certifications.vue
@@ -2,33 +2,42 @@
   <BackToTop />
   <Navigation />
 
-
-  <div class="paragraphs_under_intro_banner">
-    <div class="welcome_to_ampp">
-      <h2>Welcome to AMPP CROATIA CHAPTER</h2>
-      <p><span style="color: #ee2737;">AMPP Croatia Chapter</span> it is a non-profit association of professionals who operate within Croatian territory with the aim of promoting the protection and performance of materials. AMPP Croatia Chapter represents the Croatian expression of an international reality AMPP Association for Materials Protection and Performance, based in Houston Texas and affiliated offices around the world. AMPP is the largest global community of corrosion and coating experts.</p>
-      <NuxtLink to="/" class="find_out_more_button">Find out more</NuxtLink>
-    </div>
-    <div class="our_mission">
-      <h2>Our Mission</h2>
-      <p>Protect and safeguard assets through education, certification, accreditation, innovation and standardization. We work to ensure that our members have access to the best resources available in the field of material and performance protection, helping to strengthen professional skills and promote best practices in the sector.</p>
-      <NuxtLink to="/" class="find_out_more_button">Find out more</NuxtLink>
-    </div>
+  <div class="courses_and_certifications_intro_banner">
+          <div class="slide-inner">
+            <img class="img__ForHomePage" src="https://a.storyblok.com/f/286902983623857/1600x600/fa1457e9ca/courses_and_certifications.jpg"/>
+          </div>
   </div>
 
-  <div class="board_and_student_chapter_pictures">
-    <div class="board">
-      <h1>BOARD</h1>
-      <p>Extending knowledge about corrosion and its prevention, attracting the attention of those with an education...</p>
-      <NuxtLink to="/" class="find_out_more_button">See more</NuxtLink>
+    <div class="courses_and_certifications_text">
+      <h1>AMPP - Courses and Certifications</h1>
+
+      <p>AMPP has developed several courses on what corrosion is, how to prevent it through choice of materials, cathodic protection and protective coatings (coating).</p>
+
+      <p>The courses are aimed at different industrial sectors such as <span> Naval </span>, <span> Oil&Gas </span>, <span> Civil </span>, <span> Nuclear </span>, etc.</p>
+
+      <p>
+      It is possible to consult all the courses on the following page on the AMPP website <NuxtLink to="https://www.ampp.org/education/education-resources/courses-by-program">(see)</NuxtLink></p>
+
+      <p>In Croatia AMPP has stipulated an exclusive contract for training courses with IMC engineering srl; for any request for information on AMPP courses in Croatia please contact the <NuxtLink to="/contact">IMC training (click here)</NuxtLink></p>
     </div>
 
-    <div class="student_chapter">
-      <h1>STUDENT CHAPTER</h1>
-      <NuxtLink to="/" class="find_out_more_button">See more</NuxtLink>
+    <div class="courses_and_certifications_links_separator">
+      <div class="courses_and_certifications_links" v-for="(link,index) in links"       :key="index" >
+          <div :class="link.class">
+          <NuxtLink :to="link.to" class="courses_and_certifications_links_link">
+          <img :src="link.background">
+          </NuxtLink>
+          <div class="courses_and_certifications_links_text">
+          <h2>{{ link.title }}</h2>
+          <p>{{ link.paragraf }}</p>
+          <NuxtLink :to="link.to" class="courses_and_certifications_find_out_more_button">{{ link.button_text }}</NuxtLink>
+          </div>
+          </div>
+      </div>
     </div>
-  </div>
-  <button class="find_out_more_button_decoration">Find out more</button>
+  
+  <button class="find_out_more_button_decoration">See also</button>
+
   <div class="find_out_more_banner">
     <div class="splideDiv_see_more">
       <Splide
@@ -37,7 +46,7 @@
       >
         <SplideSlide v-for="(more, index) in seeMore" :key="index">
           <NuxtLink class="slide-inner-see_more" :to="more.to">
-            <img class="img__ForHomePage" :src="more.url" :alt="'cert-' + index" />
+            <img class="img__ForHomePage" :src="more.photo" :alt="'cert-' + index" />
             <h2> {{ more.title }}</h2>
             <p> {{ more.paragraf }}</p>
           </NuxtLink>
@@ -54,36 +63,58 @@
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 
-const photos = [
-  { url: "https://a.storyblok.com/f/286902983623857/1600x600/9f18d0ea37/banner_photo1.jpg" },
-  { url: "https://a.storyblok.com/f/286902983623857/1600x600/527acd0d1d/banner_photo2.jpg" },
-];
-
 const seeMore = [
   {
-     url: "https://a.storyblok.com/f/286902983623857/480x270/479e1a5bb5/sponsor.jpg",
-     title: "Sponsor & Exhibitors Prospectus",
-     paragraf: "4th Conference & Expo - Genoa 2026",
-     to: "/link"
+     photo: "https://a.storyblok.com/f/286902983623857/480x360/b9c0216f68/ampp_croatia_chapter.jpg",
+     title: "AMPP Croatia Chapter",
+     paragraf: "",
+     to: "/ampp-croatia/ampp-croatia-chapter"
    },
-  { 
-    url: "https://a.storyblok.com/f/286902983623857/480x270/ba640a0ff3/exhibiting.jpg",
-    title: "Who's Exhibiting?",
-    paragraf: "Take a closer look at the top players showing their solutions.",
-    to: "/link2"
-  },
-  { 
-    url: "https://a.storyblok.com/f/286902983623857/480x270/1253216aec/meetoursponsors.jpg",
-    title: "Meet Our Sponsor",
-    paragraf: "Discover the partners powering this event",
-    to: "/link3"
-  },
-  { 
-    url: "https://a.storyblok.com/f/286902983623857/480x270/7a1d277804/abstractt.jpg",
-    title: "Call for Abstract",
-    paragraf: "",
-    to: "/link4"
-  },
+  {
+     photo: "https://a.storyblok.com/f/286902983623857/480x360/565e6307d0/our_mission.jpg",
+     title: "Our Mission",
+     paragraf: "",
+     to: "/ampp-croatia/our-mission"
+   },
+  {
+     photo: "https://a.storyblok.com/f/286902983623857/480x360/0505aaa9a0/ampp_croatia_board.jpg",
+     title: "Board",
+     paragraf: "Extend knowledge about corrosion and its prevention, attracting the attention of those with appropriate education and specialization.",
+     to: "/ampp-croatia/board"
+   },
+  {
+     photo: "https://a.storyblok.com/f/286902983623857/480x360/4da830ff35/professional_ethics.jpg",
+     title: "Professional Ethics",
+     paragraf: "",
+     to: "/ampp-croatia/professional-ethics"
+   },
+];
+
+const links = [
+  {
+     title: "2025 course calendar",
+     to: "https://www.imc-quorum.com/easyNews/NewsLeggi.asp?NewsID=25",
+     class: "courses_and_certifications_links_div",
+     background: "https://a.storyblok.com/f/286902983623857/480x270/1bc820e585/course_calendar.jpg",
+     paragraf: "In Croatia",
+     button_text: "INFO & RECORDINGS"
+   },
+  {
+     title: "Calendar of all AMPP courses",
+     to: "https://www.ampp.org/education/education-resources/courses-by-program/general-corrosion/ethics-for-the-corrosion-professional",
+     class: "courses_and_certifications_links_div_2nd",
+     background: "https://a.storyblok.com/f/286902983623857/480x270/4620cb4aec/all_courses.jpg",
+     paragraf: "In the world",
+     button_text: "SEARCH"
+   },
+   {
+     title: "Search for certified professionals",
+     to: "https://nace.useclarus.com/view/verify",
+     class: "courses_and_certifications_links_div",
+     background: "https://a.storyblok.com/f/286902983623857/480x270/5f179174cd/certified_professionals.jpg",
+     paragraf: "If you are looking for certified people for a project or want to verify its certifications",
+     button_text: "LOOK FOR IT HERE"
+   },
 ];
 
 const splideOptions = {
@@ -110,5 +141,11 @@ const splideOptions2 = {
   rewind: true,
   arrows: false,
   pagination: true,
+  breakpoints: {
+    768: {
+      perPage: 1,
+    },
+  },
 };
+
 </script>
